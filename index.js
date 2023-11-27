31cd4b87bbec92f75b0e858ccfef01d4d6853153
Alinabrbr <lina.lina.malinaa@gmail.com>
2023-11-27 22:53:45 +0400

    add popup

---
+++ index.js
@@ -0,0 +1,12 @@
+const editButton = document.querySelector(".profile__edit-button");
+editButton.addEventListener('click', (openedPopup));
+
+function openedPopup () {
+    const popup = document.querySelector(".form");
+    popup.getElementsByClassName("form");
+    popup.className = "form_opened";
+}
+
+
+
+
